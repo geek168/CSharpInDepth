08ab3fae4303586666b3d6548a6190d83a5d3c61
geek168 <geek168@outlook.com>
2016-12-13 14:24:33 +0800

    Add Chapter Two

--- ChapterTwo/Program.cs
+++ ChapterTwo/Program.cs
@@ -4,14 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ChapterOne
+namespace ChapterTwo
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            Console.ReadLine();
         }
     }
 }
